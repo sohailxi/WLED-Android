087b9e3728c776c2df1d0f97ef0121d99224cd38
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-11-22 23:02:00 -0500

    Remove displayName function from DeviceWithState

--- app/src/main/java/ca/cgagnier/wlednativeandroid/service/websocket/DeviceWithState.kt
+++ app/src/main/java/ca/cgagnier/wlednativeandroid/service/websocket/DeviceWithState.kt
@@ -9,10 +9,4 @@ import ca.cgagnier.wlednativeandroid.model.wledapi.DeviceStateInfo
 class DeviceWithState(val device: Device) {
     val stateInfo: MutableState<DeviceStateInfo?> = mutableStateOf(null)
     val isWebsocketConnected: MutableState<Boolean> = mutableStateOf(false)
-
-    fun displayName(): String {
-        // TODO: copied from WLED-Desktop, but UI logic should probably not be mixed
-        //  in the business logic (or whatever this is called)
-        return device.customName ?: device.originalName ?: "(New Device)"
-    }
 }
